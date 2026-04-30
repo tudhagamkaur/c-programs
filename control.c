6c6f631bbf883ce0a60584a9d04047d84b7468a2
tudhagamkaur <tudhagam01@gmail.com>
2026-04-30 19:37:56 +0530

    added control statement program using if-else

---
+++ control.c
@@ -0,0 +1,17 @@
+// program for if and else
+#include<stdio.h>
+int main()
+    {
+    int a;
+    printf("enter your age a");
+    scanf("%d",&a);
+    if (a>18)
+    {
+        printf("congratulations you are eligible for voting");
+    }
+    else
+    {
+        printf("opps..you are not eligible");
+    }
+    return 0;
+    }
